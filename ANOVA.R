f97852175a8b7b5d1770c39cb616805726d80f4a
KedraMichal <52571532+KedraMichal@users.noreply.github.com>
2019-12-04 19:27:32 +0100

    code

--- ANOVA.R
+++ ANOVA.R
@@ -1,6 +1,6 @@
 library(ggplot2)
-library(psych)     # funkcja describeBy()
-library(lawstat)   # funkcja levene.test()
+library(psych)     
+library(lawstat)   
 library(dunn.test)
 library(tseries)
 
@@ -47,7 +47,22 @@ shapiro_results
 
 
 births_anova <- aov(BWT~AGE*RACE, data = births)
-births_anova
-summary(births_anova)
+
+summary_anova<- summary(births_anova)
+summary_anova
+str(summary_anova)
+summary_anova[[1]][1,1]
 
 
+eta.age<- summary_anova[[1]][1,2]/(summary_anova[[1]][1,2]+summary_anova[[1]][4,2])
+eta.race<- summary_anova[[1]][2,2]/(summary_anova[[1]][2,2]+summary_anova[[1]][4,2])
+eta.race_age<- summary_anova[[1]][3,2]/(summary_anova[[1]][3,2]+summary_anova[[1]][4,2])   
+
+
+omega.age<- (summary_anova[[1]][1,2]-summary_anova[[1]][1,1]*summary_anova[[1]][4,3])/(summary_anova[[1]][4,3]+sum(summary_anova[[1]][,2]))
+omega.race<- (summary_anova[[1]][2,2]-summary_anova[[1]][2,1]*summary_anova[[1]][4,3])/(summary_anova[[1]][4,3]+sum(summary_anova[[1]][,2]))
+omega.race_age<- (summary_anova[[1]][3,2]-summary_anova[[1]][3,1]*summary_anova[[1]][4,3])/(summary_anova[[1]][4,3]+sum(summary_anova[[1]][,2]))
+
+tuckey<- TukeyHSD(births_anova, conf.level = 0.99)
+plot(tuckey,las =1, col = "blue" )
+                               
